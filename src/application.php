5fd92ddd658c8d27b770f0ea0d59701b79a4cbe5
John (Arcus) <john@arcuscompliance.com>
2018-05-27 18:10:03 +0100

    Ensure Symfony console is required by composer.
    See: #1

---
+++ src/application.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php
+<?php
+// application.php
+
+require __DIR__.'../../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+
+// ... register commands
+//$application->add(new GenerateAdminCommand());
+
+$application->run();
