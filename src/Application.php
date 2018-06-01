ab4a5accfc86235b939071f6fe83a7b0d5c4be93
Igor Mukhin <igor.mukhin@gmail.com>
2018-06-01 14:55:02 +0300

    Added: Some kind of "hardcoded" commands autodiscovery

--- src/Application.php
+++ src/Application.php
@@ -8,12 +8,24 @@ use Forikal\Tools\Command\HelloWorldCommand;
 class Application extends BaseApplication
 {
     const NAME = 'Forikal Tools';
-    const VERSION = '0.1.0';
+    const VERSION = '0.1.1';
 
     public function __construct($name = self::NAME, $version = self::VERSION)
     {
         parent::__construct($name, $version);
 
         $this->add(new HelloWorldCommand());
+
+        if (class_exists('Forikal\PingDrive\Commands\PingDriveCommand')) {
+            $this->add(
+                new \Forikal\PingDrive\Commands\PingDriveCommand()
+            );
+        }
+
+        if (class_exists('Forikal\GsheetXml\Command\GsheetToXmlCommand')) {
+            $this->add(
+                new \Forikal\GsheetXml\Command\GsheetToXmlCommand()
+            );
+        }
     }
 }
